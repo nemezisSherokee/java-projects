9e5baa457acf19257bdd2facd0dae30021986240
kamdoum <lary_xyahoo.fr>
2019-02-11 13:21:33 +0100

    second changes to new new branch

--- gitproject/src/main/java/com/project/git/gitproject/App.java
+++ gitproject/src/main/java/com/project/git/gitproject/App.java
@@ -56,3 +56,5 @@ public class App
 //add-nice-printer-for-string change 1 new branch
 //add-nice-printer-for-string change 1 new new branch
 // first changes to new new branch
+//add-nice-printer-for-string change 2 new new branch
+//second changes to new new branch
