3d19e932adef3f7bfdd66b9bb01333eba704e6e1
kamdoum <lary_xyahoo.fr>
2019-02-11 13:10:13 +0100

    first changes to new branch

--- forkBranch.sh
+++ forkBranch.sh
@@ -1,18 +1,27 @@
 clear
 
 newbranch=$1
-
-test
+newbranch2=$2
+ 
 test -z "$1" && {
 	 newbranch="branch1_$(date +%s)";
 	 echo "$newbranch"
 	}
 
-	git checkout -b $newbranch add-nice-printer-for-string
+test -z "$2" && {
+	 newbranch2="branch2_$(date +%s)";
+	 echo "$newbranch2"
+	}
+
+git checkout -b $newbranch add-nice-printer-for-string
 echo "//add-nice-printer-for-string change 1 new branch" >> ./gitproject/src/main/java/com/project/git/gitproject/App.java
 git add *
 git commit -m "first changes to new branch"
 
+git checkout -b $newbranch2 $newbranch
+echo "//add-nice-printer-for-string change 1 new new branch" >> ./gitproject/src/main/java/com/project/git/gitproject/App.java
+git add *
+git commit -m "first changes to new new branch"
 
 git checkout add-nice-printer-for-string
 echo "//add-nice-printer-for-string change 1 original branch" >> ./gitproject/src/main/java/com/project/git/gitproject/App.java
@@ -31,4 +40,24 @@ echo "//add-nice-printer-for-string change 2 new branch" >> ./gitproject/src/mai
 git add *
 git commit -m "second changes to new branch"
 
+git checkout  $newbranch2
+echo "//add-nice-printer-for-string change 2 new new branch" >> ./gitproject/src/main/java/com/project/git/gitproject/App.java
+git add *
+git commit -m "second changes to new new branch"
 	
+
+git checkout add-nice-printer-for-string
+echo "//add-nice-printer-for-string change 3 original branch" >> ./gitproject/src/main/java/com/project/git/gitproject/App.java
+git add *
+git commit -m "third changes to add-nice-printer-for-string"
+
+
+git checkout  $newbranch
+echo "//add-nice-printer-for-string change 3 new branch" >> ./gitproject/src/main/java/com/project/git/gitproject/App.java
+git add *
+git commit -m "third changes to new branch"
+
+git checkout  $newbranch2 
+echo "//add-nice-printer-for-string change 2 new new branch" >> ./gitproject/src/main/java/com/project/git/gitproject/App.java
+git add *
+git commit -m "third changes to new new branch"
